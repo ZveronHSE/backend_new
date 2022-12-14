3da50d86c0ab50bdebe846b99a2856909a118e75
sofia.terekhova <sofiamaria.terekho@aliexpress.ru>
2022-12-15 00:07:19 +0300

    [ZV-224] update without protobuf

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import com.google.protobuf.gradle.builtins
-import com.google.protobuf.gradle.generateProtoTasks
-import com.google.protobuf.gradle.id
-import com.google.protobuf.gradle.ofSourceSet
-import com.google.protobuf.gradle.plugins
-import com.google.protobuf.gradle.protobuf
-import com.google.protobuf.gradle.protoc
-
 plugins {
     idea
 
     id("org.springframework.boot")
     id("io.spring.dependency-management")
-    id("com.google.protobuf") version "0.8.18"
     kotlin("jvm")
     kotlin("plugin.spring")
 }
@@ -35,7 +26,6 @@ subprojects {
         plugin("kotlin")
         plugin("org.jetbrains.kotlin.jvm")
         plugin("org.jetbrains.kotlin.plugin.spring")
-        plugin("com.google.protobuf")
         plugin("org.springframework.boot")
         plugin("io.spring.dependency-management")
     }
@@ -45,8 +35,6 @@ subprojects {
         implementation("io.grpc:grpc-kotlin-stub:$grpcKotlinVersion")
         implementation("io.grpc:grpc-stub:$grpcVersion")
         implementation("io.grpc:grpc-core:$grpcVersion")
-        implementation("io.grpc:grpc-protobuf:$grpcVersion")
-        implementation("com.google.protobuf:protobuf-kotlin:$protobufVersion")
     }
 
     group = rootProject.group
@@ -58,31 +46,6 @@ subprojects {
         kotlinOptions {
             freeCompilerArgs = listOf("-Xjsr305=strict")
             jvmTarget = "17"
-        }
-    }
-
-    protobuf {
-        protoc {
-            artifact = "com.google.protobuf:protoc:$protobufVersion"
-        }
-        plugins {
-            id("grpc") {
-                artifact = "io.grpc:protoc-gen-grpc-java:$grpcVersion"
-            }
-            id("grpckt") {
-                artifact = "io.grpc:protoc-gen-grpc-kotlin:$grpcKotlinVersion:jdk8@jar"
-            }
-        }
-        generateProtoTasks {
-            ofSourceSet("main").forEach {
-                it.plugins {
-                    id("grpc")
-                    id("grpckt")
-                }
-                it.builtins {
-                    id("kotlin")
-                }
-            }
         }
     }
 }
