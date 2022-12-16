29e6b376ea5a99b3eeb26de2a2cec126437e65c0
WolfAlm <60275667+WolfAlm@users.noreply.github.com>
2022-12-16 19:34:43 +0300

    [ZV-225] Добавлены зависимости от контрактов (#2)
    Co-authored-by: WolfAlm

--- address_service/build.gradle.kts
+++ address_service/build.gradle.kts
@@ -2,5 +2,8 @@ plugins {
 }
 
 dependencies {
+    implementation("com.github.zveronHSe.contract:address:1.0.0")
+    implementation("com.github.zveronHSe.contract:parameter:1.0.0")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
