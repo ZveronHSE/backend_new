2a274ee18a0523f8442644ecc6551b28f99a7184
Kochik Dmitry <dakochik@edu.hse.ru>
2023-01-31 21:38:27 +0300

    я не понимаю почему тесты падают

--- blacklist_service/src/main/kotlin/ru/zveron/BlacklistServiceApplication.kt
+++ blacklist_service/src/main/kotlin/ru/zveron/BlacklistServiceApplication.kt
@@ -3,9 +3,11 @@ package ru.zveron
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableJpaRepositories(basePackages = ["ru.zveron"])
 class BlacklistServiceApplication
 
 fun main(args: Array<String>) {
