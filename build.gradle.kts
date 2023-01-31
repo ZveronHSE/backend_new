70e7414389428837926cf33e572a647f6e4ceb32
sofia.terekhova <sofiamaria.terekho@aliexpress.ru>
2023-01-31 23:51:31 +0300

    [ZV-000] fix tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -135,12 +135,9 @@ subprojects {
         testImplementation("org.springframework.boot:spring-boot-starter-test:$springVersion")
         testImplementation("org.testcontainers:postgresql:$testcontainersVersion")
         testImplementation("org.testcontainers:junit-jupiter:$testcontainersVersion")
-        testImplementation("org.testcontainers:r2dbc:$testcontainersVersion")
         testImplementation("io.kotest:kotest-assertions-core-jvm:5.2.2")
         testImplementation("org.assertj:assertj-core:3.22.0")
         testImplementation("io.mockk:mockk:1.13.3")
-        implementation("com.ninja-squad:springmockk:4.0.0")
-
     }
 
     group = rootProject.group
