f64784d07a8dc55bf6116a20ae88af6e29508683
Kochik Dmitry <dakochik@edu.hse.ru>
2023-01-31 21:19:47 +0300

    я не понимаю почему тесты падают

--- blacklist_service/src/main/kotlin/ru/zveron/repository/BlacklistRepository.kt
+++ blacklist_service/src/main/kotlin/ru/zveron/repository/BlacklistRepository.kt
@@ -1,9 +1,11 @@
 package ru.zveron.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 import org.springframework.transaction.annotation.Transactional
 import ru.zveron.entity.BlacklistRecord
 
+@Repository
 interface BlacklistRepository : JpaRepository<BlacklistRecord, BlacklistRecord.BlacklistKey> {
     fun existsById_OwnerUserIdAndId_ReportedUserId(ownerUserId: Long, reportedUserId: Long): Boolean
 
