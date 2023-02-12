88c4db525cd1ab1ec2216548067cb92fb5f021f1
KIRILL PIQUE <kirill.pique@alibaba-inc.com>
2023-02-12 23:58:20 +0300

    ZV-321: init storage service

---
+++ object-storage/src/test/kotlin/ru/zveron/objectstorage/ObjectStorageApplicationTests.kt
@@ -0,0 +1,12 @@
+package ru.zveron.objectstorage
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ObjectStorageApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
