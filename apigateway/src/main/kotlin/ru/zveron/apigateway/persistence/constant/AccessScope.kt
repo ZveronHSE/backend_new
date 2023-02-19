5e4f4ec71e06db14ca4d25ae8447ae64707b929b
KIRILL PIQUE <kirill.pique@alibaba-inc.com>
2023-02-19 20:36:06 +0300

    ZV-320: json creator

--- apigateway/src/main/kotlin/ru/zveron/apigateway/persistence/constant/AccessScope.kt
+++ apigateway/src/main/kotlin/ru/zveron/apigateway/persistence/constant/AccessScope.kt
@@ -1,6 +1,18 @@
 package ru.zveron.apigateway.persistence.constant
 
+import com.fasterxml.jackson.annotation.JsonCreator
+
 enum class AccessScope {
     ANY,
     BUYER,
+    ;
+
+    companion object {
+        @JvmStatic
+        @JsonCreator
+        fun byAlias(alias: String) {
+            AccessScope.values().singleOrNull { it.name.equals(alias, true) }
+                ?: error("Non existent ContextType for alias=$alias")
+        }
+    }
 }
