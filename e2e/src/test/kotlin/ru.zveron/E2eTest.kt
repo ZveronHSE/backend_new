30b5eeebbf3f716a0cc76a7d4e4636b1256aae6f
Kochik Dmitry <dakochik@edu.hse.ru>
2023-03-11 14:02:00 +0300

    Убрал хардкод адреса машинки, добавил ридми, доавил volume в бд

--- e2e/src/test/kotlin/ru.zveron/E2eTest.kt
+++ e2e/src/test/kotlin/ru.zveron/E2eTest.kt
@@ -10,16 +10,19 @@ import ru.zveron.contract.apigateway.ApigatewayServiceGrpcKt
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.minutes
 
+private const val SERVER_HOST_PROPERTY_NAME = "zveron_server_host"
+private const val SERVER_PORT_PROPERTY_NAME = "zveron_server_port"
 private const val DEFAULT_RETRIES_NUMBER = 5
 private const val DEFAULT_RETRY_DELAY_MINUTES = 2
 
 abstract class E2eTest {
 
     private val client = ApigatewayServiceGrpcKt.ApigatewayServiceCoroutineStub(
-        ManagedChannelBuilder.forAddress("zveron.ru", 80).usePlaintext().build()
+        ManagedChannelBuilder.forAddress(
+            System.getenv(SERVER_HOST_PROPERTY_NAME),
+            System.getenv(SERVER_PORT_PROPERTY_NAME).toInt()
+        ).usePlaintext().build()
     )
-
-    // System.getenv
 
     protected fun assertCallSucceeded(
         apiGatewayRequest: ApiGatewayRequest,
