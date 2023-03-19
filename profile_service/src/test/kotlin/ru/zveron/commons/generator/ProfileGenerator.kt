ce145fd64845dbd27a1ae68437608b09fb3777a7
Kochik Dmitry <dakochik@edu.hse.ru>
2023-03-19 14:08:24 +0300

    [ZV-353] Change dummy address id in profile

--- profile_service/src/test/kotlin/ru/zveron/commons/generator/ProfileGenerator.kt
+++ profile_service/src/test/kotlin/ru/zveron/commons/generator/ProfileGenerator.kt
@@ -7,7 +7,7 @@ import java.time.Instant
 
 object ProfileGenerator {
 
-    fun generateProfile(lastSeen: Instant, addressId: Long = -1, addPassword: Boolean = false) = Profile(
+    fun generateProfile(lastSeen: Instant, addressId: Long = 0, addPassword: Boolean = false) = Profile(
         name = generateString(20),
         surname = generateString(20),
         imageId = generateLongId(),
