c1159f11177fa0318c4884affa1c72bd17c6bce6
KIRILL PIQUE <kirill.pique@alibaba-inc.com>
2023-03-23 11:10:15 +0300

    ZV-296: adjust logging interceptor to log everything

--- apigateway/src/main/kotlin/ru/zveron/apigateway/utils/LogstashHelper.kt
+++ apigateway/src/main/kotlin/ru/zveron/apigateway/utils/LogstashHelper.kt
@@ -1,10 +1,13 @@
 package ru.zveron.apigateway.utils
 
 import com.google.protobuf.DynamicMessage
+import com.google.protobuf.GeneratedMessageV3
 import com.google.protobuf.util.JsonFormat
 
 object LogstashHelper {
     private val protoJsonPrinter = JsonFormat.printer().omittingInsignificantWhitespace()
 
     fun DynamicMessage.toJson(): String = protoJsonPrinter.print(this)
+
+    fun <T : GeneratedMessageV3> T.toJson(): String = protoJsonPrinter.print(this)
 }
