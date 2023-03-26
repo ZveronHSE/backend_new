18eb228d76710e8eedc4a3f1c1d9029d41179e3b
KIRILL PIQUE <kirill.pique@alibaba-inc.com>
2023-03-26 22:16:24 +0300

    ZV-000: up contract version and add logs

--- object-storage/build.gradle.kts
+++ object-storage/build.gradle.kts
@@ -13,7 +13,7 @@ dependencies {
 
     implementation("aws.sdk.kotlin:s3:0.20.2-beta")
     implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.10")
-    implementation("com.github.zveronHSe.contract:objectstorage:ab5398c0ce")
+    implementation("com.github.zveronHSe.contract:objectstorage:1.9.3")
 
     implementation("aws.smithy.kotlin:http-client-engine-okhttp-jvm:0.15.2")
 }
