a058e04e0bd6d5e1ea8e82b604f93bb999afa4b3
KIRILL PIQUE <kirill.pique@alibaba-inc.com>
2023-04-30 17:39:54 +0300

    [ZV-401] : redundant plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,36 +9,27 @@ pluginManagement {
         id("org.springframework.boot") version springVersion
         id("io.spring.dependency-management") version springDependencyManagementPluginVersion
 
-        id("org.jooq.codegen") version "3.15.2"
-
         kotlin("jvm") version kotlinVersion
         kotlin("plugin.spring") version kotlinVersion
         kotlin("plugin.jpa") version kotlinVersion
     }
-
-    pluginManagement {
-        repositories {
-            gradlePluginPortal()
-            maven { url = uri("https://plugins.gradle.org/m2/") }
-        }
-    }
 }
 
 include(
-        "address_service",
-        "parameter_service",
-        "blacklist_service",
-        "favorites_service",
-        "blacklist_service",
-        "service_registry",
-        "apigateway",
-        "lot_service",
-        "profile_service",
-        "auth-service",
-        "object-storage",
-        "push_notification",
-        "e2e",
-        "chat_service",
-        "order-service",
+    "address_service",
+    "parameter_service",
+    "blacklist_service",
+    "favorites_service",
+    "blacklist_service",
+    "service_registry",
+    "apigateway",
+    "lot_service",
+    "profile_service",
+    "auth-service",
+    "object-storage",
+    "push_notification",
+    "e2e",
+    "chat_service",
+    "order-service",
     "specialist-service",
 )
