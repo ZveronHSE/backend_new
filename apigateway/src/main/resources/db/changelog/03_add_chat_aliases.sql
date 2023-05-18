af20b74530167fcd4a09ae890b7b044d4e3805a6
Kochik Dmitry <dakochik@edu.hse.ru>
2023-05-18 23:50:38 +0300

    Move changelog script to init script

--- apigateway/src/main/resources/db/changelog/03_add_chat_aliases.sql
+++
@@ -1,5 +0,0 @@
---changeset dakochik:chat_aliases
-
-insert into public.method_metadata (alias, service_name, grpc_service_name, grpc_method_name, access_scope)
-values ('chatPing', 'chat-service', 'PingServiceExternal', 'Ping', 'ANY'),
-       ('bidiChatRoute', 'chat-service', 'ChatServiceExternal', 'BidiChatRoute', 'BUYER');
