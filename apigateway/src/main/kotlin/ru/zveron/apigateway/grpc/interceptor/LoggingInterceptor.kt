0b03643ca0364d706dba1031768e3a544465546c
sofia.terekhova <sofiamaria.terekho@aliexpress.ru>
2023-05-31 18:52:31 +0300

    [ZV-000] change logs

--- apigateway/src/main/kotlin/ru/zveron/apigateway/grpc/interceptor/LoggingInterceptor.kt
+++ apigateway/src/main/kotlin/ru/zveron/apigateway/grpc/interceptor/LoggingInterceptor.kt
@@ -1,93 +1,43 @@
 package ru.zveron.apigateway.grpc.interceptor
 
 import com.google.protobuf.GeneratedMessageV3
-import io.grpc.ForwardingServerCall
-import io.grpc.ForwardingServerCallListener
-import io.grpc.Metadata
 import io.grpc.ServerCall
-import io.grpc.ServerCallHandler
-import io.grpc.ServerInterceptor
-import io.opentelemetry.api.trace.Span
 import mu.KLogging
 import net.devh.boot.grpc.server.interceptor.GrpcGlobalServerInterceptor
 import net.logstash.logback.marker.Markers
 import net.logstash.logback.marker.Markers.append
 import net.logstash.logback.marker.Markers.appendRaw
-import org.apache.commons.lang3.StringUtils
-import org.jboss.logging.MDC
 import ru.zveron.apigateway.utils.LogstashHelper.toJson
 import ru.zveron.contract.apigateway.ApiGatewayRequest
-import ru.zveron.contract.apigateway.ApigatewayResponse
+import ru.zveron.library.grpc.interceptor.logging.LoggingServerInterceptor
 import ru.zveron.library.grpc.interceptor.model.MethodType
-import ru.zveron.library.grpc.interceptor.tracing.TracingHelper
 
 @GrpcGlobalServerInterceptor
-class LoggingInterceptor : ServerInterceptor {
+class LoggingInterceptor : LoggingServerInterceptor() {
 
     companion object : KLogging() {
         const val CALL_TYPE = "callType"
     }
 
-    final override fun <ReqT : Any, RespT : Any> interceptCall(
-        call: ServerCall<ReqT, RespT>,
-        headers: Metadata?,
-        next: ServerCallHandler<ReqT, RespT>,
-    ): ServerCall.Listener<ReqT> {
-        // Костыль но мне похуй я ебала думать что тут пошло не так и почему нужно ВОТ так делать.
-        MDC.put(TracingHelper.traceIdKey.name(), Span.current().spanContext.traceId)
+    override fun <ReqT, RespT> logMessage(methodType: MethodType, call: ServerCall<ReqT, RespT>, message: Any) {
+        if (methodType == MethodType.REQUEST) {
+            val protoMessage = message as? ApiGatewayRequest
 
-        val wrappedCall = object : ForwardingServerCall.SimpleForwardingServerCall<ReqT, RespT>(call) {
-            override fun sendMessage(message: RespT) {
-                logResponse(message)
-                super.sendMessage(message)
+            protoMessage?.let {
+                val marker = Markers.aggregate(
+                    //todo sensitive masking
+                    appendRaw("requestBody", protoMessage.requestBody.toStringUtf8()),
+                    append("methodAlias", protoMessage.methodAlias),
+                    append(CALL_TYPE, MethodType.REQUEST),
+                )
+
+                logger.info(marker) { }
+            } ?: kotlin.run {
+                message as GeneratedMessageV3
+                logger.info(appendRaw("request", message.toJson())) { "Not an apigateway request" }
             }
-        }
-
-        return object : ForwardingServerCallListener.SimpleForwardingServerCallListener<ReqT>(
-            next.startCall(
-                wrappedCall,
-                headers,
-            )
-        ) {
-            override fun onMessage(message: ReqT) {
-                logRequest(message)
-                super.onMessage(message)
-            }
-        }
-    }
-
-    private fun <ReqT> logRequest(message: ReqT) {
-        val protoMessage = message as? ApiGatewayRequest
-
-        protoMessage?.let {
-            val marker = Markers.aggregate(
-                //todo sensitive masking
-                appendRaw("requestBody", protoMessage.requestBody.toStringUtf8()),
-                append("methodAlias", protoMessage.methodAlias),
-                append(CALL_TYPE, MethodType.REQUEST),
-            )
-
-            logger.info(marker) { }
-        } ?: kotlin.run {
-            message as GeneratedMessageV3
-            logger.info(appendRaw("request", message.toJson())) { "Not an apigateway request" }
-        }
-    }
-
-    private fun <RespT> logResponse(message: RespT) {
-        val response = message as? ApigatewayResponse
-
-        response?.let {
-            response.responseBody.toStringUtf8().let { StringUtils.normalizeSpace(it) }
-            //todo sensitive masking
-            val marker = Markers.aggregate(
-                appendRaw("responseBody", response.toJson()),
-                append(CALL_TYPE, MethodType.RESPONSE),
-            )
-            logger.info(marker) { }
-        } ?: kotlin.run {
-            message as GeneratedMessageV3
-            logger.info(append("response", message.toJson())) { "Not an apigateway response" }
+        } else {
+            logger.info { "send response" }
         }
     }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -144,7 +144,7 @@ subprojects {
         liquibaseRuntime(sourceSets.getByName("main").output)
 
         // Платформенная либа:
-        implementation("com.github.zveronHse:platform-library:1.1.1")
+        implementation("com.github.zveronHse:platform-library:1.1.2")
 
         // Тесты
         testImplementation("io.grpc:grpc-testing:$grpcVersion")
